a975193f0a2fcc5eb939fde73dde16adc1543be5
shweta352 <sdpawar0902@gmail.com>
2025-02-17 12:50:44 +0530

    add initial files

---
+++ src/components/CityItem.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import styles from "./cityItem.module.css";
+import { Link } from 'react-router-dom';
+import { useCities } from '../contexts/CitiesContext';
+
+const formatDate = (date) =>
+  new Intl.DateTimeFormat("en", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  }).format(new Date(date));
+
+
+function CityItem({city}) {
+  function handleClick(e){
+    e.preventDefault();
+    deleteCity(id);
+  }
+
+  const {currentCity, deleteCity} = useCities();
+  const  {cityName, emoji, date, id, position} = city;
+
+  return (
+    <li>
+      <Link  className={`${styles.cityItem} ${id===currentCity.id ? styles["cityItem--active"] : ""}`} 
+      to={`${id}?lat=${position.lat}&lng=${position.lng}`}>
+      <span className={styles.emoji}>{emoji}</span>
+      <h3 className={styles.name}>{cityName}</h3>
+      <time className={styles.date}>({formatDate(date)})</time>
+      <button className={styles.deleteBtn} onClick={handleClick}>&times;</button>
+      </Link>
+    </li>
+  );
+  
+}
+
+export default CityItem;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,30 @@
+import styles from "./Sidebar.module.css";
+import {Outlet} from "react-router-dom";
+import Logo from "./Logo";
+import AppNav from "./AppNav";
+
+
+import React from 'react'
+
+
+
+function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+        <Logo />
+        <AppNav />
+
+        <Outlet />
+
+        
+        <footer className={styles.footer}>
+            <p className={styles.copyright}>
+                &copy; Copyright {new Date().getFullYear()}by WorldWise Inc.
+            </p>
+        </footer>
+
+    </div>
+  )
+}
+
+export default Sidebar
